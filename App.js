44f2b55df9fa8943dc6ce8edffb0ae4569617dc3
dhruvilgabani <princegabani01@gmail.com>
2024-12-13 14:11:38 -0600

    Added Designation by gabani

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Dhruvil Gabani</Text>
+      <Text>Graphics Designer</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#98C1D9',
     alignItems: 'center',
     justifyContent: 'center',
   },
